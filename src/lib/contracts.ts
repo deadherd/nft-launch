aa5aae53dfbb702b402f97f01d94a69e8e054aa6
Leo Black <dev@n3t.run>
2025-06-23 16:17:17 -0700

    Production

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -2,4 +2,4 @@
 
 // central registry for deployed contract addresses used across the app
 
-export const MAIN_NFT_CONTRACT = '0x2e51a8FdC067e415CFD5d00b9add5C6Af72d676c'
+export const MAIN_NFT_CONTRACT = '0x76065074344824a3201E46b84FA6611384bD7E92'
