256cd1d5b50fa4c2ae139cd788cf0d43b03fc169
Leo Black <dev@n3t.run>
2025-06-24 02:50:47 -0700

    Production changes

--- src/hooks/useTotalSupply.ts
+++ src/hooks/useTotalSupply.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { JsonRpcProvider, Contract } from 'ethers'
-import CraftedCollectionABI from '@/abi/MadeForRats.json'
+import MadeForRatsABI from '@/abi/MadeForRats.json'
 import { MAIN_NFT_CONTRACT } from '@/lib/contracts'
 
 export default function useTotalSupply() {
@@ -12,7 +12,7 @@ export default function useTotalSupply() {
     const fetchSupply = async () => {
       try {
         const provider = new JsonRpcProvider(`https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`)
-        const contract = new Contract(MAIN_NFT_CONTRACT, CraftedCollectionABI.abi, provider)
+        const contract = new Contract(MAIN_NFT_CONTRACT, MadeForRatsABI.abi, provider)
         const result = await contract.totalSupply()
         setSupply(Number(result))
       } catch (err) {
